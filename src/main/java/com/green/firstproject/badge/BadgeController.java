61316cb68fdf6a90b5d8da0c61be85250f91b8ea
성수천 <3598ysm@gmail.com>
2023-06-21 09:58:07 +0900

    mapper

--- src/main/java/com/green/firstproject/badge/BadgeController.java
+++ src/main/java/com/green/firstproject/badge/BadgeController.java
@@ -1,4 +1,10 @@
 package com.green.firstproject.badge;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/badge")
 public class BadgeController {
+
 }
--- src/main/java/com/green/firstproject/badge/model/BadgeEntity.java
+++ src/main/java/com/green/firstproject/badge/model/BadgeEntity.java
@@ -1,4 +1,10 @@
 package com.green.firstproject.badge.model;
 
+import lombok.Data;
+
+@Data
 public class BadgeEntity {
+    private int ibadge;
+    private int points;
+    private String pic;
 }
