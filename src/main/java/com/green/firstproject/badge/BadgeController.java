64ac527072cc748eebbefeb6a3a4ec136c16b635
성수천 <3598ysm@gmail.com>
2023-06-21 12:56:59 +0900

    mapper

--- src/main/java/com/green/firstproject/badge/BadgeController.java
+++ src/main/java/com/green/firstproject/badge/BadgeController.java
@@ -1,10 +1,19 @@
 package com.green.firstproject.badge;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.green.firstproject.badge.model.BadgeInsDto;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/badge")
 public class BadgeController {
+private final BadgeService service;
 
+    public BadgeController(BadgeService service) {
+        this.service = service;
+    }
+    @PostMapping
+    public int insBadge(@RequestPart MultipartFile pic, @RequestBody BadgeInsDto dto){
+        return service.insPic(dto);
+    }
 }
