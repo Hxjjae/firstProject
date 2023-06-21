b33de5bf90ccfdba4d4639fce631b78a148ec085
진혁재 <jhj02201@gmail.com>
2023-06-21 09:58:41 +0900

    각 테이블별 클래스생성 및 패키지구분

--- src/main/java/com/green/firstproject/task/TaskService.java
+++ src/main/java/com/green/firstproject/task/TaskService.java
@@ -1,4 +1,12 @@
 package com.green.firstproject.task;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class TaskService {
+
+    private final TaskMapper mapper;
 }
