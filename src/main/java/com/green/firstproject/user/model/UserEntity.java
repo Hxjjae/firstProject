768c921ce4303e2dd2ce3b84b0a917335ea66b81
배성현 <zkti12345@naver.com>
2023-06-22 10:56:36 +0900

    yaml

--- src/main/java/com/green/firstproject/user/model/UserEntity.java
+++ src/main/java/com/green/firstproject/user/model/UserEntity.java
@@ -1,4 +1,11 @@
 package com.green.firstproject.user.model;
 
+import lombok.Data;
+
+@Data
 public class UserEntity {
+    private int iuser;
+    private  String name;
+    private  int totalpoint;
+    private  int ibadge;
 }
---
+++ src/main/java/com/green/firstproject/user/model/UserInsDto.java
@@ -0,0 +1,8 @@
+package com.green.firstproject.user.model;
+
+import lombok.Data;
+
+@Data
+public class UserInsDto {
+    private String name;
+}
