1528e2d95246afea68875a3fb9e0703a233ca9f9
진혁재 <jhj02201@gmail.com>
2023-06-27 14:35:01 +0900

    카테고리, 스티커

---
+++ src/main/java/com/green/firstproject/category/CategoryMapper.java
@@ -0,0 +1,14 @@
+package com.green.firstproject.category;
+
+import com.green.firstproject.category.model.CategoryInsDto;
+import com.green.firstproject.category.model.CategorySelDto;
+import com.green.firstproject.category.model.CategoryVo;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface CategoryMapper {
+    int insCategory(CategoryInsDto dto);
+    List<CategoryVo> selCategory();
+}
--- src/main/java/com/green/firstproject/sticker/StickerController.java
+++ src/main/java/com/green/firstproject/sticker/StickerController.java
@@ -7,7 +7,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,17 +23,18 @@ public class StickerController {
     @Autowired
     private final StickerService service;
 
-    @PostMapping
+    @PostMapping(name = "/추가", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE} )
     @Operation(summary = "스티커 추가")
-    int postSticker(@RequestBody StickerInsDto dto) {
-        return service.insSticker(dto);
+    int postSticker(@RequestPart MultipartFile pic
+                    , @RequestParam int level) {
+        StickerInsDto dto = new StickerInsDto();
+        dto.setLevel(level);
+        return service.insSticker(pic,dto);
     }
 
     @GetMapping
     @Operation(summary = "스티커 보기")
-    List<StickerVo> getSticker(@RequestParam StickerSelDto dto) {
-        service.selSticker(dto);
-        List<StickerVo> list = new ArrayList<>();
-        return list;
+    List<StickerVo> getSticker() {
+        return service.selSticker();
     }
 }
