32229441e8fa04f3d5928f001770ed774f206294
진혁재 <jhj02201@gmail.com>
2023-06-30 14:44:42 +0900

    Timer 수정 및 mapper 생성

--- src/main/java/com/green/firstproject/timer/TimerController.java
+++ src/main/java/com/green/firstproject/timer/TimerController.java
@@ -1,14 +1,15 @@
 package com.green.firstproject.timer;
 
 import com.green.firstproject.timer.model.TimerInsDto;
+import com.green.firstproject.timer.model.TimerUpdDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/timer")
+@Tag(name = "타이머")
 @RequiredArgsConstructor
 
 public class TimerController {
@@ -16,7 +17,14 @@ public class TimerController {
     private final TimerService SERVICE;
 
     @PostMapping
+    @Operation(summary = "타이머 추가")
     public int insTimer(@RequestBody TimerInsDto dto) {
         return SERVICE.insTimer(dto);
+    }
+
+    @PatchMapping
+    @Operation(summary = "타이머 수정 및 스티커 추가")
+    public int patchTimer(@RequestBody TimerUpdDto dto) {
+        return SERVICE.updTimer(dto);
     }
 }
