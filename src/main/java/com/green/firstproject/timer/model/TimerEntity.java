c0323b3970f963a9343f07dd60710a8ac35afea4
진혁재 <jhj02201@gmail.com>
2023-07-07 16:43:45 +0900

    timer 수정 및 추가

---
+++ src/main/java/com/green/firstproject/timer/model/TimerEntity.java
@@ -0,0 +1,12 @@
+package com.green.firstproject.timer.model;
+
+import lombok.Data;
+
+@Data
+public class TimerEntity {
+    private int itimer;
+    private String deadLine;
+    private int studyLine;
+    private int iuser;
+    private int isticker;
+}
