df0c2cb562536bbd2096e80243f227d2d775f31f
진혁재 <jhj02201@gmail.com>
2023-07-12 10:15:39 +0900

    Email operation 추가

--- src/main/java/com/green/firstproject/user/UserController.java
+++ src/main/java/com/green/firstproject/user/UserController.java
@@ -19,20 +19,15 @@ public class UserController {
 
 
     @PostMapping
-    @Operation(summary = "프로필 이름등록", description = "name : 이름<br>"+"목표와 사진은 기본적으로 null값")
+    @Operation(summary = "프로필 이름등록", description = "name : 이름<br>" + "목표와 사진은 기본적으로 null값")
     public int postUser(@RequestBody UserInsDto dto) {
         return service.insUser(dto);
     }
 
 
-
-
-
-
-
     @PutMapping(value = "/profile/{iuser}")
-    @Operation(summary = "프로필 이름, 목표 수정, 이메일 수정",description = "name : 이름<br>"+"ovjective : 목표<br>"+"iuser : 유저 pk값<br>"+"email : 이메일")
-    public int allPutUser(@PathVariable int iuser,@RequestBody UserAllDto dto) {
+    @Operation(summary = "프로필 이름, 목표 수정, 이메일 수정", description = "name : 이름<br>" + "ovjective : 목표<br>" + "iuser : 유저 pk값<br>" + "email : 이메일")
+    public int allPutUser(@PathVariable int iuser, @RequestBody UserAllDto dto) {
         UserEntity entity = new UserEntity();
         entity.setIuser(iuser);
         entity.setName(dto.getName());
@@ -41,12 +36,7 @@ public class UserController {
         return service.upAllUser(entity);
 
 
-
     }
-
-
-
-
 
 
     @GetMapping("/alllist")
